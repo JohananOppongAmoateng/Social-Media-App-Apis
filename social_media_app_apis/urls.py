e6f640aee0a4138a13de1335868dc1e84861526d
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-08-25 03:47:37 +0000

    user authentication

---
+++ social_media_app_apis/urls.py
@@ -0,0 +1,54 @@
+"""
+URL configuration for social_media_app_apis project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.schemas import get_schema_view
+
+...
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Social Media API",
+      default_version='v1.0.0',
+      description="This a list of API for a simple social media app",
+    #   terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="johananoppongamoateng2001@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path("admin/", admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path("posts/", include("posts.urls")),
+    path("groups/",include("groups.urls")),
+    path("followers/",include("followers.urls")),
+    path("search",include("search.urls")),
+]
